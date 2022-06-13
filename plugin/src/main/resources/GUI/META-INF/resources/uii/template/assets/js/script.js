2450b03a2fc881d1e989a970cd82dd2ca1c8fa04
Robert Sehr <robert.sehr@intranda.com>
2022-06-13 07:54:01 +0200

    Refactor frontend JS; refs #22594

---
+++ plugin/src/main/resources/GUI/META-INF/resources/uii/template/assets/js/script.js
@@ -0,0 +1,31 @@
+$("document").ready(function () {
+  console.log('ready')
+
+  // Scripts for modals can be found in `resources/composites/modals.xhtml`
+
+  // Activate Tooltips
+  var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"], [data-toggle="tooltip"]'))
+  var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl)
+  });
+
+
+  /*
+  // show inline help
+  const form = document.querySelector('form');
+  form.addEventListener('click', handleClick);
+
+  function handleClick(e) {
+      if(e.target.classList.contains('form-icons__button--help')) {
+          const formElement = e.target.parentElement.parentElement
+          const helpText = formElement.querySelector('.help-text');
+          helpText.classList.toggle('vis-hidden');
+      }
+  }
+
+  */
+
+
+
+
+});
