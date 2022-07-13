40c6148e759b91300750d8f4033574461f8416d7
Jakob Epler <jakob.epler@intranda.com>
2022-07-13 12:38:43 +0200

    Add css build system + clean up css structure

--- plugin/src/main/resources/GUI/META-INF/resources/uii/template/assets/css/build-css.sh
+++ plugin/src/main/resources/GUI/META-INF/resources/uii/template/assets/css/build-css.sh
@@ -6,9 +6,9 @@ SRC_CUSTOM='src/index.scss'
 DST_CUSTOM='dist/custom.css'
 
 # include these load paths
-BS_SASS="${HOME}/git/goobi-workflow/Goobi/src/main/webapp/node_modules/bootstrap/scss/"
-CUSTOM_SASS="${HOME}/git/goobi-workflow/Goobi/src/main/webapp/uii/templatePG/css/src/"
+BS_SASS="${HOME}/projects/webapp/node_modules/bootstrap/scss/"
+CUSTOM_SASS="${HOME}/projects/webapp/sass/src/"
 
-# run sass task
+# run sass task and include load paths (using an env var)
 SASS_PATH=${BS_SASS}:${CUSTOM_SASS} sass --watch ${SRC_BS}:${DST_BS} ${SRC_CUSTOM}:${DST_CUSTOM}
 
