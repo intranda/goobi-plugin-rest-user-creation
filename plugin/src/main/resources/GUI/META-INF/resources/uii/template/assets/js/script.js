747094ca6daad8c2e2af69999f66740115652a8d
Jakob Epler <jakob.epler@intranda.com>
2022-10-11 15:01:38 +0200

    Wrap tooltip activation into a function; #23717

--- plugin/src/main/resources/GUI/META-INF/resources/uii/template/assets/js/script.js
+++ plugin/src/main/resources/GUI/META-INF/resources/uii/template/assets/js/script.js
@@ -1,28 +1,17 @@
-$("document").ready(function () {
+// Scripts for modals can be found in `resources/composites/modals.xhtml`
+// => "Inline Help" + page help
 
-  // Scripts for modals can be found in `resources/composites/modals.xhtml`
-  // => "Inline Help" + page help
+// Activate Bootstrap 5 Tooltips
+function tooltipsInit() {
 
-  // Activate Tooltips
   var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"], [data-toggle="tooltip"]'))
   var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
     return new bootstrap.Tooltip(tooltipTriggerEl)
   });
 
+}
 
-  /*
-  // show inline help
-  const form = document.querySelector('form');
-  form.addEventListener('click', handleClick);
-
-  function handleClick(e) {
-      if(e.target.classList.contains('form-icons__button--help')) {
-          const formElement = e.target.parentElement.parentElement
-          const helpText = formElement.querySelector('.help-text');
-          helpText.classList.toggle('vis-hidden');
-      }
-  }
-  */
-
+$("document").ready(function () {
+  tooltipsInit();
 
 });
